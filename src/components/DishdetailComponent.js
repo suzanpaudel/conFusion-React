20aff1ea250ec4953ed1f8455d9320b8c57a2ec1
Sujan Paudel <sujan2000paudel@gmail.com>
2020-06-09 13:44:42 +0545

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -17,6 +17,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
@@ -175,8 +176,24 @@ function RenderComments({ comments, addComment, dishId }) {
   }
 }
 const DishdetailComponent = (props) => {
-  const dish = props.dish;
-  if (dish != null) {
+  // const dish = props.dish;
+  if (props.isLoading) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null) {
     return (
       <div className='container'>
         <div className='row'>
